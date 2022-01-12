bcc4d2bd531cb852e20de2dc4bdec7451dbd01bb
abu3d <35095551+abu3d@users.noreply.github.com>
2022-01-12 15:07:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,11 +5,14 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.199.220/192.168.199.1/g' openwrt/package/base-files/files/bin/config_generate
+#1. 修改默认的IP
+sed -i 's/192.168.1.1/192.168.199.220/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#2. 修改主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Routes'' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+#3. 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#4. 版本号里显示一个自己的名字
+sed -i "s/OpenWrt /Abu3d-Pve $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
